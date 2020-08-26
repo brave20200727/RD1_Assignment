28e53790269a7ef087ed9bae8059b5f30741c28c
Brave <brave_chen@mail.chungyo.net>
2020-08-26 11:23:37 +0800

    新增功能將氣象局取得的資料存入資料庫中

--- myApi.php
+++ myApi.php
@@ -21,19 +21,22 @@
         while($row = mysqli_fetch_assoc($result)) {
             $allData[] = $row;
         }
-        echo json_encode($allData);    
+        echo json_encode($allData);
+        mysqli_close($dbLink);
     }
 
     function getOneCity() {
         global $dbLink;
         $cityId = $_GET["cityId"];
-        $selectCommand = "SELECT cityName FROM cities WHERE cityId = $cityId";
-        $result = mysqli_query($dbLink, $selectCommand);
+        $sqlCommand = "SELECT cityName FROM cities WHERE cityId = $cityId";
+        $result = mysqli_query($dbLink, $sqlCommand);
         $row = mysqli_fetch_assoc($result);
         echo json_encode($row);
+        mysqli_close($dbLink);
     }
 
     function getWeatherInfoForInternet() {
+        global $dbLink;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-237F0446-FCE5-4CD0-A343-4C4B52E42D65&format=JSON&sort=time");
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -41,13 +44,13 @@
         $result = curl_exec($ch);
         $answer = json_decode($result);
         $cityWeather = $answer->records->locations[0]->location[0];
-        // var_dump($cityWeather);
         $cityName = $cityWeather->locationName;
+
         $weatherElementPoP12H = $cityWeather->weatherElement[0]; // 12小時降雨機率
         $weatherElementT = $cityWeather->weatherElement[1]; // 平均溫度
         $weatherElementRH = $cityWeather->weatherElement[2]; // 平均相對濕度
         $weatherElementWS = $cityWeather->weatherElement[4]; // 風速
-        $weatherElementED = $cityWeather->weatherElement[13]; // 風向
+        $weatherElementWD = $cityWeather->weatherElement[13]; // 風向
         $weatherElementMaxAT = $cityWeather->weatherElement[5]; // 最高體感溫度
         $weatherElementMinAT = $cityWeather->weatherElement[11]; // 最低體感溫度
         $weatherElementMaxT = $cityWeather->weatherElement[12]; // 最高溫度
@@ -55,11 +58,20 @@
         $weatherElementWx = $cityWeather->weatherElement[6]; // 天氣現象
         $weatherElementUVI = $cityWeather->weatherElement[9]; // 紫外線指數
         $weatherElementTd = $cityWeather->weatherElement[14]; // 平均露點溫度
-        // foreach($cityWeather AS $temp) {
-        //     echo $temp->locationName . "<br>";
-        // }
-        // var_dump($weatherElementTime);
-        echo $result;
+
+        $elementArray = $weatherElementPoP12H->time;
+        foreach($elementArray AS $oneElement) {
+            $startTime = $oneElement->startTime;
+            $endTime = $oneElement->endTime;
+            $elementValue = $oneElement->elementValue[0]->value;
+            if($elementValue == " ") {
+                $elementValue = 0;
+            }
+            $elementUnit = $oneElement->elementValue[0]->measures;
+            $sqlCommand = "INSERT IGNORE INTO PoP12H(cityId, startTime, endTime, elementValue, unit) VALUES ((SELECT cityId FROM cities WHERE cityName = '$cityName'), '$startTime', '$endTime', '$elementValue', '$elementUnit')";
+            mysqli_query($dbLink, $sqlCommand);
+        }
         curl_close($ch);
+        mysqli_close($dbLink);
     }
 ?>
