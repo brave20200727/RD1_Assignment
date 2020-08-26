f6e6e70528dc321f8574f6a97272b8c841863a75
Brave <brave_chen@mail.chungyo.net>
2020-08-26 10:07:02 +0800

    將所有抓資料的部分都改為透過自己寫出的API，也就是都透過myApi.php

--- myApi.php
+++ myApi.php
@@ -35,13 +35,31 @@
 
     function getWeatherInfoForInternet() {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-237F0446-FCE5-4CD0-A343-4C4B52E42D65&sort=time");
+        curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-237F0446-FCE5-4CD0-A343-4C4B52E42D65&format=JSON&sort=time");
         curl_setopt($ch, CURLOPT_HEADER, false);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // $result = curl_exec($ch);
-        // $answer = json_decode($result);
-        // var_dump($answer);
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        $answer = json_decode($result);
+        $cityWeather = $answer->records->locations[0]->location[0];
+        // var_dump($cityWeather);
+        $cityName = $cityWeather->locationName;
+        $weatherElementPoP12H = $cityWeather->weatherElement[0]; // 12小時降雨機率
+        $weatherElementT = $cityWeather->weatherElement[1]; // 平均溫度
+        $weatherElementRH = $cityWeather->weatherElement[2]; // 平均相對濕度
+        $weatherElementWS = $cityWeather->weatherElement[4]; // 風速
+        $weatherElementED = $cityWeather->weatherElement[13]; // 風向
+        $weatherElementMaxAT = $cityWeather->weatherElement[5]; // 最高體感溫度
+        $weatherElementMinAT = $cityWeather->weatherElement[11]; // 最低體感溫度
+        $weatherElementMaxT = $cityWeather->weatherElement[12]; // 最高溫度
+        $weatherElementMinT = $cityWeather->weatherElement[8]; // 最低溫度
+        $weatherElementWx = $cityWeather->weatherElement[6]; // 天氣現象
+        $weatherElementUVI = $cityWeather->weatherElement[9]; // 紫外線指數
+        $weatherElementTd = $cityWeather->weatherElement[14]; // 平均露點溫度
+        // foreach($cityWeather AS $temp) {
+        //     echo $temp->locationName . "<br>";
+        // }
+        // var_dump($weatherElementTime);
+        echo $result;
         curl_close($ch);
     }
 ?>
